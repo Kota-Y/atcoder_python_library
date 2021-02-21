38ccca23b6c9c62dcaf3ca9bd4b0c08c21e84bdd
kota-yamashita <mcz-0604-htt@outlook.com>
2021-02-21 16:25:15 +0900

    素因数分解

---
+++ math/prime_factorization.py
@@ -0,0 +1,15 @@
+def prime_factorize(n: int) -> list:
+    return_list = []
+    while n % 2 == 0:
+        return_list.append(2)
+        n //= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            return_list.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        return_list.append(n)
+    return return_list
