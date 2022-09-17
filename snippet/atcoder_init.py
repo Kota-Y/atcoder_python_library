1713927240ffb55c64daaf16ae8e29f03af40dac
kota-yamashita <mcz-0604-htt@outlook.com>
2022-09-17 19:00:00 +0900

    snipetの更新

--- snippet/atcoder_init.py
+++ snippet/atcoder_init.py
@@ -1,12 +1,15 @@
 import sys, re
-from math import ceil, floor, sqrt, pi, factorial, gcd
+from string import ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits
+from operator import add, sub, mul, mod, xor
+from math import sqrt, pi, factorial, gcd, sin, cos, atan2,degrees, radians
 from copy import deepcopy
-from collections import Counter, deque
+from collections import Counter, deque, defaultdict
 from heapq import heapify, heappop, heappush
-from itertools import accumulate, product, combinations, combinations_with_replacement
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
 from bisect import bisect, bisect_left, bisect_right
 from functools import reduce
 from decimal import Decimal, getcontext
+from array import array
 # input = sys.stdin.readline 
 def i_input(): return int(input())
 def i_map(): return map(int, input().split())
@@ -20,11 +23,11 @@ def s_row(N): return [s_input() for _ in range(N)]
 def s_row_str(N): return [s_list() for _ in range(N)]
 def s_row_list(N): return [list(s_input()) for _ in range(N)]
 def lcm(a, b): return a * b // gcd(a, b)
+def ceil(a, b): return (a + b - 1) // b
+def floor(a, b): return a // b
 sys.setrecursionlimit(10 ** 6)
 INF = float('inf')
 MOD = 10 ** 9 + 7
-num_list = []
-str_list = []
 
 def main():
     n = i_input()
